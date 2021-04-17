e1ec824e934b6cf20c1f747c410bf90383179b71
zhangjian <zhangjian02@souche.com>
2021-04-17 21:49:20 +0800

    feature: 登录

---
+++ src/components/Login.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form :model="loginForm"
+               :rules="loginFormRules"
+               abel-width="0px"
+               class="login_form"
+               ref="loginFormRef">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user"
+                    v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-3702mima"
+                    v-model="loginForm.password"
+                    type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary"
+                     @click="login">登录</el-button>
+          <el-button type="info"
+                     @click="resetForm('loginFormRef')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 数据绑定对象
+      loginForm: {
+        username: '2@qq.com',
+        password: '123456'
+      },
+      loginFormRules: {
+        // 验证用户名
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+        ],
+        // 验证密码
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) {
+          // 未校验通过
+          return
+        }
+        var { data: response } = await this.$http.post('/v1/login', this.loginForm)
+        if (response.code !== 200) {
+          // 登录失败
+          return this.open(response.message)
+        }
+        // 登录成功
+        this.$message.success('登录成功')
+        window.sessionStorage.setItem('token', response.token)
+        this.$router.push('/home')
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
+    open (message) {
+      alert(message, '登录失败', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${action}`
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: white;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100px;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 10px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'
+import { Form, FormItem, Input, Button, Alert, Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://localhost:10006/'
+
+Vue.prototype.$http = axios
+Vue.prototype.$message = Message
+
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Alert)
 
 Vue.config.productionTip = false
 
